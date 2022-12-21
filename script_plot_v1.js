4d5e345ff24c14d4b7d6b3cd9e1104ce6d6f739e
anh231000 <72401605+anh231000@users.noreply.github.com>
2022-12-21 17:58:18 +0700

    main

--- script_plot_v1.js
+++ script_plot_v1.js
@@ -1,10 +1,11 @@
-var layout = {xaxis: {title: "t (s)"}, yaxis: {title: "T (K)"}, title: "Clab N"};
-var layout1 = {xaxis: {title: "t (s)"}, yaxis: {title: "T (K)"}, title: "Clab N"};
-var layout2 = {xaxis: {title: "t (s)"}, yaxis: {title: "T (K)"}, title: "Clab N"};
+var layout = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 1850"};
+var layout1 = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 1322"};
+var layout2 = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 19299"};
+var layout3 = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 19310"};
 Plotly.newPlot("myPlot",layout);
 Plotly.newPlot("myPlot1",layout1);  
 Plotly.newPlot("myPlot2",layout2);  
-
+Plotly.newPlot("myPlot3",layout3);  
 
 firebase
 .database()
@@ -21,52 +22,51 @@ firebase
         if (snap.val().control == 1){
 
             let value = snap.val().value;  
+            let value1 = snap.val().value1;  
+            value1 = Math.round(value1)/10;
 
             const data1 = [];
             const data2 = [];
             const data3 = [];
-            const labelsa = [];
-            const labelsb = [];
-            const labelsc = [];
+            const data4 = [];
+            const stt = [];
   
             let k=2;
 
-            url1 = 'https://raw.githubusercontent.com/lamtacta2/WAAM/main/Data/data1' + value.toString() + ".csv";
-            url2 = 'https://raw.githubusercontent.com/lamtacta2/WAAM/main/Data/data2' + value.toString() + ".csv";
-            url3 = 'https://raw.githubusercontent.com/lamtacta2/WAAM/main/Data/data' + value.toString() + ".csv";
-
+            url1 = 'https://raw.githubusercontent.com/lamtacta2/WAAM/main/Data/data' + value.toString() + value1.toString() + ".csv";
             
             let workbook1 = XLSX.read(await (await fetch(url1)).arrayBuffer());
-            let workbook2 = XLSX.read(await (await fetch(url2)).arrayBuffer());
-            let workbook3 = XLSX.read(await (await fetch(url3)).arrayBuffer());
-  
   
            function data_update(k){
             for(let i = 2; i < k; i++){
-                const locale2 = "A"+i;
-                const locale1 = "B"+i;               
+                const locale1 = "A"+i;
+                const locale2 = "B"+i; 
+                const locale3 = "C"+i;  
+                const locale4 = "D"+i;                
+                const locale5 = "E"+i;
 
-                data1[i-2] = workbook1.Sheets.Sheet1[locale1].v.slice(1,workbook1.Sheets.Sheet1[locale1].v.length-1);
-                data2[i-2] = workbook2.Sheets.Sheet1[locale1].v.slice(1,workbook2.Sheets.Sheet1[locale1].v.length-1);
-                data3[i-2] = workbook3.Sheets.Sheet1[locale1].v.slice(1,workbook3.Sheets.Sheet1[locale1].v.length-1);
-                labelsa[i-2] = workbook1.Sheets.Sheet1[locale2].v;
-                labelsb[i-2] = workbook2.Sheets.Sheet1[locale2].v;
-                labelsc[i-2] = workbook3.Sheets.Sheet1[locale2].v;
+                data1[i-2] = workbook1.Sheets.Sheet1[locale2].v;
+                data2[i-2] = workbook1.Sheets.Sheet1[locale3].v;
+                data3[i-2] = workbook1.Sheets.Sheet1[locale4].v;
+                data4[i-2] = workbook1.Sheets.Sheet1[locale5].v;
+                stt[i-2] = workbook1.Sheets.Sheet1[locale1].v;
             }}
   
             // Define Data
-            var data = [{x: labelsa, y: data1, mode:"lines"}];
-            var datax1 = [{x: labelsb, y: data2, mode:"lines"}];
-            var datax2 = [{x: labelsc, y: data3, mode:"lines"}];
+            var data = [{x: stt, y: data1, mode:"lines"}];
+            var datax1 = [{x: stt, y: data2, mode:"lines"}];
+            var datax2 = [{x: stt, y: data3, mode:"lines"}];
+            var datax3 = [{x: stt, y: data4, mode:"lines"}];
 
            function update(){
 
-             if (k<1978){
+             if (k<420){
               k = k+1;
-             if (k < 1978){data_update(k);}
+             if (k < 420){data_update(k);}
              Plotly.newPlot("myPlot", data, layout);
              Plotly.newPlot("myPlot1", datax1, layout1);
-             Plotly.newPlot("myPlot2", datax2, layout2);
+             Plotly.newPlot("myPlot2", datax2, layout2); 
+             Plotly.newPlot("myPlot3", datax3, layout3);
              requestAnimationFrame(update);
 
            }}
