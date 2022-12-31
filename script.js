1a8efc32c9f2bf35cfa5707d873cd64b834d18d8
anh231000 <72401605+anh231000@users.noreply.github.com>
2022-12-31 14:13:29 +0700

    main

--- script.js
+++
@@ -1,5 +0,0 @@
-var layout = {xaxis: {title: "t (s)"}, yaxis: {title: "T (K)"}, title: "Clab N"};
-var layout1 = {xaxis: {title: "t (s)"}, yaxis: {title: "T (K)"}, title: "Clab N"};
-
-Plotly.newPlot("myPlot",layout);
-Plotly.newPlot("myPlot1",layout1);  
--- script_plot_v1.js
+++ script_plot_v1.js
@@ -1,11 +1,10 @@
-var layout = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 1850"};
-var layout1 = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 1322"};
-var layout2 = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 19299"};
-var layout3 = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 19310"};
-Plotly.newPlot("myPlot",layout);
-Plotly.newPlot("myPlot1",layout1);  
-Plotly.newPlot("myPlot2",layout2);  
-Plotly.newPlot("myPlot3",layout3);  
+var layout = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}};
+var layout1 = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}};
+
+var demo = [{x: 0, y: 0, mode:"lines"}];
+
+Plotly.newPlot("myPlot", demo, layout);
+Plotly.newPlot("myPlot1", demo, layout1);  
 
 firebase
 .database()
@@ -21,12 +20,14 @@ firebase
 
         if (snap.val().control == 1){
 
+            let point = snap.val().point;
+            let point1 = snap.val().point1;
+
             let value = snap.val().value;  
             let value1 = snap.val().value1;  
             value = value.toFixed(1); 
             value1 = value1.toFixed(0); 
-            console.log(value1.toString());
-            console.log(value.toString());
+
             const data1 = [];
             const data2 = [];
             const data3 = [];
@@ -38,7 +39,7 @@ firebase
             url1 = 'https://raw.githubusercontent.com/lamtacta2/WAAM/main/Data/data' + value1.toString() + value.toString() + ".csv";
             
             let workbook1 = XLSX.read(await (await fetch(url1)).arrayBuffer());
-  
+
            function data_update(k){
             for(let i = 2; i < k; i++){
                 const locale1 = "A"+i;
@@ -54,22 +55,43 @@ firebase
                 stt[i-2] = workbook1.Sheets.Sheet1[locale1].v;
             }}
   
-            // Define Data
             var data = [{x: stt, y: data1, mode:"lines"}];
             var datax1 = [{x: stt, y: data2, mode:"lines"}];
-            var datax2 = [{x: stt, y: data3, mode:"lines"}];
-            var datax3 = [{x: stt, y: data4, mode:"lines"}];
+
+            if(point == 0){
+              data = [{x: stt, y: data1, mode:"lines"}];
+              layout = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 1850"};
+            } else if(point == 1){
+              data = [{x: stt, y: data2, mode:"lines"}];
+              layout = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 1322"};
+            }else if(point == 2){
+              data = [{x: stt, y: data3, mode:"lines"}];
+              layout = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 19299"};
+            } else{
+              data = [{x: stt, y: data4, mode:"lines"}];
+              layout = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 19310"};
+            }
+
+            if(point1 == 0){
+              datax1 = [{x: stt, y: data1, mode:"lines"}];
+              layout1 = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 1850"};
+            } else if(point1 == 1){
+              datax1 = [{x: stt, y: data2, mode:"lines"}];
+              layout1 = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 1322"};
+            }else if(point1 == 2){
+              datax1 = [{x: stt, y: data3, mode:"lines"}];
+              layout1 = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 19299"};
+            } else{
+              datax1 = [{x: stt, y: data4, mode:"lines"}];
+              layout = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 19310"};
+            }
 
            function update(){
-
              if (k<420){
               k = k+1;
              if (k < 420){data_update(k);}
              Plotly.newPlot("myPlot", data, layout);
              Plotly.newPlot("myPlot1", datax1, layout1);
-             Plotly.newPlot("myPlot2", datax2, layout2); 
-             Plotly.newPlot("myPlot3", datax3, layout3);
              requestAnimationFrame(update);
-
            }}
            requestAnimationFrame(update);}})(); }) 
